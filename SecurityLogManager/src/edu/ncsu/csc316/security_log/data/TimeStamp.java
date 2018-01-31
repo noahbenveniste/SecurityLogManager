10af0b0f829ea06683783aebfb4e92661e74a270
Noah Benveniste <Noah Benveniste@PC>
2018-01-31 18:34:32 -0500

    Implemented and tested TimeStamp constructor

--- SecurityLogManager/src/edu/ncsu/csc316/security_log/data/TimeStamp.java
+++ SecurityLogManager/src/edu/ncsu/csc316/security_log/data/TimeStamp.java
@@ -6,9 +6,75 @@ package edu.ncsu.csc316.security_log.data;
  */
 public class TimeStamp implements Comparable<TimeStamp> {
 
+    private String month;
+    private String day;
+    private String year;
+    private String hour;
+    private String minute;
+    private String second;
+    private boolean isAM;
+    
+    /**
+     * 
+     * @param s
+     */
+    public TimeStamp( String s ) {
+        // s is of the form "month/day/year hour:minute:second(AM or PM)"
+        String[] dateTime = s.split("\\s+");
+        String date = dateTime[0];
+        String time = dateTime[1];
+        
+        String[] monthDayYear = date.split("/");
+        this.month = monthDayYear[0];
+        this.day = monthDayYear[1];
+        this.year = monthDayYear[2];
+        
+        String[] hourMinSec = time.split(":");
+        this.hour = hourMinSec[0];
+        this.minute = hourMinSec[1];
+        
+        String temp = hourMinSec[2];
+        StringBuilder sb = new StringBuilder();
+        this.second = sb.append( temp.charAt(0) ).append( temp.charAt(1) ).toString();
+        sb = new StringBuilder();
+        String dayOrNight = sb.append( temp.charAt(2) ).append( temp.charAt(3) ).toString();
+        this.isAM = dayOrNight.equals("AM");
+    }
+    
+    public String getMonth() {
+        return month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public boolean isAM() {
+        return isAM;
+    }
+
+    /**
+     * 
+     */
     @Override
-    public int compareTo(TimeStamp arg0) {
-        // Sorts first 
+    public int compareTo( TimeStamp other ) {
+        // Sorts in order by year, month, day, AM vs PM, hour, minute, second
         return 0;
     }
 
---
+++ SecurityLogManager/test/edu/ncsu/csc316/security_log/data/TimeStampTest.java
@@ -0,0 +1,32 @@
+package edu.ncsu.csc316.security_log.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ * @author Noah Benveniste
+ */
+class TimeStampTest {
+
+    private static final String t1 = "04/26/2017 12:33:15PM";
+    private static final String t2 = "11/20/2017 11:38:22AM";
+    private static final String t3 = "08/04/2016 06:57:34AM";
+    
+    /**
+     * 
+     */
+    @Test
+    void testTimeStamp() {
+        TimeStamp ts1 = new TimeStamp(t1);
+        assertEquals("04", ts1.getMonth());
+        assertEquals("26", ts1.getDay());
+        assertEquals("2017", ts1.getYear());
+        assertEquals("12", ts1.getHour());
+        assertEquals("33", ts1.getMinute());
+        assertEquals("15", ts1.getSecond());
+        assertFalse(ts1.isAM());
+    }
+
+}
