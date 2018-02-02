e3af834f3297420f00910ad38cc515b078927ef8
Noah Benveniste <Noah Benveniste@PC>
2018-02-02 16:41:34 -0500

    Added comparison functionality to TimeStamp and LogEntry objects
    - Added compareTo() and equals() methods that allow LogEntries to be
    sorted first by time, then action, then resource and then user if
    necessary.

--- SecurityLogManager/src/edu/ncsu/csc316/security_log/data/LogEntry.java
+++ SecurityLogManager/src/edu/ncsu/csc316/security_log/data/LogEntry.java
@@ -48,13 +48,81 @@ public class LogEntry implements Comparable<LogEntry> {
         return this.resource;
     }
     
+    public TimeStamp getTimeStamp() {
+        return this.timeStamp;
+    }
+    
     /**
      * 
      */
     @Override
     public int compareTo( LogEntry other ) {
         // Sorts in order by time stamp, action and resource
-        return 0;
+        if (this.timeStamp.compareTo(other.getTimeStamp()) != 0) {
+            return this.timeStamp.compareTo(other.getTimeStamp());
+        } else if (!this.action.equals(other.getAction())) {
+            return this.getAction().compareTo(other.getAction());
+        } else if (!this.getResource().equals(other.getResource())) {
+            return this.getResource().compareTo(other.getResource());
+        } else if (!this.getUser().equals(other.getUser())) {
+            return this.getUser().compareTo(other.getUser());
+        } else {
+            throw new IllegalArgumentException("Cannot have duplicate log entries."); 
+        }
+    }
+    
+    
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((action == null) ? 0 : action.hashCode());
+        result = prime * result + ((resource == null) ? 0 : resource.hashCode());
+        result = prime * result + ((timeStamp == null) ? 0 : timeStamp.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LogEntry other = (LogEntry) obj;
+        if (action == null) {
+            if (other.action != null)
+                return false;
+        } else if (!action.equals(other.action))
+            return false;
+        if (resource == null) {
+            if (other.resource != null)
+                return false;
+        } else if (!resource.equals(other.resource))
+            return false;
+        if (timeStamp == null) {
+            if (other.timeStamp != null)
+                return false;
+        } else if (!timeStamp.equals(other.timeStamp))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+
+    /**
+     * Returns all logs sorted by time stamp, action, resource and then user.
+     * @return all log entries as a string
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(this.timeStamp.toString()).append(" ").append(this.user).append(" ").append(this.action).append(" ").append(this.resource).toString();
     }
 
 }
--- SecurityLogManager/src/edu/ncsu/csc316/security_log/data/TimeStamp.java
+++ SecurityLogManager/src/edu/ncsu/csc316/security_log/data/TimeStamp.java
@@ -13,12 +13,14 @@ public class TimeStamp implements Comparable<TimeStamp> {
     private String minute;
     private String second;
     private boolean isAM;
+    private String originalString;
     
     /**
      * 
      * @param s
      */
     public TimeStamp( String s ) {
+        this.originalString = s;
         // s is of the form "month/day/year hour:minute:second(AM or PM)"
         String[] dateTime = s.split("\\s+");
         String date = dateTime[0];
@@ -75,7 +77,100 @@ public class TimeStamp implements Comparable<TimeStamp> {
     @Override
     public int compareTo( TimeStamp other ) {
         // Sorts in order by year, month, day, AM vs PM, hour, minute, second
-        return 0;
+        if (!this.getYear().equals(other.getYear())) {
+            return this.getYear().compareTo(other.getYear());
+        } else if (!this.getMonth().equals(other.getMonth())) {
+            return this.getMonth().compareTo(other.getMonth());
+        } else if (!this.getDay().equals(other.getDay())) {
+            return this.getDay().compareTo(other.getDay());
+        } else if (this.isAM() != other.isAM) {
+            if (this.isAM()) { // Makes this time "greater than" the other time
+                return -1;
+            } else {
+                return 1;
+            }
+        } else if (!this.getHour().equals(other.getHour())) {
+            return this.getHour().compareTo(other.getHour());
+        } else if (!this.getMinute().equals(other.getMinute())) {
+            return this.getMinute().compareTo(other.getMinute());
+        } else if (!this.getSecond().equals(other.getSecond())) {
+            return this.getSecond().compareTo(other.getSecond());
+        } else {
+            return 0; // Equal time stamps
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((day == null) ? 0 : day.hashCode());
+        result = prime * result + ((hour == null) ? 0 : hour.hashCode());
+        result = prime * result + (isAM ? 1231 : 1237);
+        result = prime * result + ((minute == null) ? 0 : minute.hashCode());
+        result = prime * result + ((month == null) ? 0 : month.hashCode());
+        result = prime * result + ((originalString == null) ? 0 : originalString.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        result = prime * result + ((year == null) ? 0 : year.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TimeStamp other = (TimeStamp) obj;
+        if (day == null) {
+            if (other.day != null)
+                return false;
+        } else if (!day.equals(other.day))
+            return false;
+        if (hour == null) {
+            if (other.hour != null)
+                return false;
+        } else if (!hour.equals(other.hour))
+            return false;
+        if (isAM != other.isAM)
+            return false;
+        if (minute == null) {
+            if (other.minute != null)
+                return false;
+        } else if (!minute.equals(other.minute))
+            return false;
+        if (month == null) {
+            if (other.month != null)
+                return false;
+        } else if (!month.equals(other.month))
+            return false;
+        if (originalString == null) {
+            if (other.originalString != null)
+                return false;
+        } else if (!originalString.equals(other.originalString))
+            return false;
+        if (second == null) {
+            if (other.second != null)
+                return false;
+        } else if (!second.equals(other.second))
+            return false;
+        if (year == null) {
+            if (other.year != null)
+                return false;
+        } else if (!year.equals(other.year))
+            return false;
+        return true;
+    }
+
+    /**
+     * 
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        //return sb.append(month).append("/").append(day).append("/").append(year).append(" ").append(hour).append(":").append(str)
+        return originalString;
     }
 
 }
