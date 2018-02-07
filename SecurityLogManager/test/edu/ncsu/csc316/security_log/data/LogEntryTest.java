fe8cc0b070f08e0e6dede34a168f04926fa0494b
Noah Benveniste <Noah Benveniste@PC>
2018-02-06 22:48:21 -0500

    Increased coverage for LogEntry

--- SecurityLogManager/test/edu/ncsu/csc316/security_log/data/LogEntryTest.java
+++ SecurityLogManager/test/edu/ncsu/csc316/security_log/data/LogEntryTest.java
@@ -16,5 +16,20 @@ public class LogEntryTest {
         assertTrue(l1.compareTo(l2) < 0);
         assertTrue(l3.compareTo(l4) < 0);
     }
+    
+    @Test
+    public void testEquals() {
+        LogEntry l1 = new LogEntry("nnbenven", "01/01/2000 12:00:00AM", "action", "resource");
+        LogEntry l2 = new LogEntry("abcd", "01/01/2000 12:00:00AM", "action", "resource");
+        LogEntry l3 = new LogEntry("nnbenven", "12/01/2000 12:00:00AM", "action", "resource");
+        LogEntry l4 = new LogEntry("nnbenven", "01/01/2000 12:00:00AM", "differentaction", "resource");
+        LogEntry l5 = new LogEntry("nnbenven", "01/01/2000 12:00:00AM", "action", "differentresource");
+        LogEntry l6 = new LogEntry("nnbenven", "01/01/2000 12:00:00AM", "action", "resource");
+        assertFalse(l1.equals(l2));
+        assertFalse(l1.equals(l3));
+        assertFalse(l1.equals(l4));
+        assertFalse(l1.equals(l5));
+        assertTrue(l1.equals(l6));
+    }
 
 }
